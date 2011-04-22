cd06b07b46cda8bfdef9275dde4033807baaedf0
DynamiteAthe <DynamiteAthe@gmail.com>
2011-04-22 18:18:43 +0000

    Added opengl defines.

--- vortex_platform_independence/include/vtx_defineconfig.h
+++ vortex_platform_independence/include/vtx_defineconfig.h
@@ -10,13 +10,17 @@
 #if defined(VTX_PLATFORM_WIN32)
 #define VTX_COMPILE_WITH_DX10
 //#define VTX_COMPILE_WITH_DX11
-//#define VTX_COMPILE_WITH_OPENGL
-#endif
+#define VTX_COMPILE_WITH_OPENGL
 
+#endif
 
 #if defined(VTX_COMPILE_WITH_DX10)
 #pragma comment(lib, "DXGI.lib")
 #pragma comment(lib, "D3D10.lib")
+#endif
+
+#if defined(VTX_COMPILE_WITH_OPENGL) && defined(VTX_PLATFORM_WIN32)
+#pragma comment(lib, "opengl32.lib")
 #endif
 
 #endif
