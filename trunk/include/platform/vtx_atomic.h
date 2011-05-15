d8f967b0e0c21c07656964bee996095dafe9ec8e
DynamiteAthe@gmail.com <DynamiteAthe@gmail.com@da90791a-ce81-3e00-88ae-c54e835546c7>
2011-05-15 19:24:45 +0000

    Added msvc specific integer typedefs.

--- trunk/include/platform/vtx_atomic.h
+++ trunk/include/platform/vtx_atomic.h
@@ -38,21 +38,31 @@ namespace platform
 #endif
 
 #define U32_SIGNIFICANT_BIT (1 << 31)
-
-	typedef unsigned char U8;
-	typedef signed char I8;
-	typedef unsigned short U16;
-	typedef signed short I16;
-	typedef unsigned int U32;
-	typedef signed int I32;
-#if defined(VTX_PLATFORM_WINDOWS)
-	typedef unsigned long long U64;
+#if defined (VTX_PLATFORM_LINUX)
+	typedef unsigned char		U8;
+	typedef signed char			I8;
+	typedef unsigned short		U16;
+	typedef signed short		I16;
+	typedef unsigned int		U32;
+	typedef signed int			I32;
+	typedef unsigned long long	U64;
+	typedef float				F32;
+#endif
+#if defined(VTX_PLATFORM_WIN32)
+	typedef unsigned __int8		U8;
+	typedef signed __int8		I8;
+	typedef unsigned __int16	U16;
+	typedef signed __int16		I16;
+	typedef unsigned __int32	U32;
+	typedef signed __int32		I32;
+	typedef unsigned __int64	U64;
+	typedef float				F32;
 #endif
 #if defined(VTX_PLATFORM_LINUX)
 	// Workaround until I find out how to get 64 bit integers on unix with g++.
-	typedef unsigned long long U64;
+	
 #endif
-	typedef float F32;
+	
 
 }
 
