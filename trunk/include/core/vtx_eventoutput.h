cca74e87a64b0a3e1034d8f321f28f5a7f01b47f
DynamiteAthe <DynamiteAthe@da90791a-ce81-3e00-88ae-c54e835546c7>
2011-06-04 09:29:16 +0000

    Forgot to -svn add- the EventOutput files in my previous commit

---
+++ trunk/include/core/vtx_eventoutput.h
@@ -0,0 +1,44 @@
+//Copyright (C) 2011 Emil Nordén
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef VTX_EVENTOUTPUT_H
+#define VTX_EVENTOUTPUT_H
+
+#include <platform/vtx_atomic.h>
+
+#include <string>
+#include <iostream>
+#include <map>
+
+
+class EventOutput
+{
+public:
+	enum E_LEVEL { E_LEVEL_VERBOSE, E_LEVEL_INFO, E_LEVEL_WARNING, E_LEVEL_ERROR, E_LEVEL_FATAL };
+	std::map<E_LEVEL, std::wstring> map;
+	EventOutput( E_LEVEL level) : reportLevel(level), outStream(&std::wcerr) {};
+	EventOutput( E_LEVEL level, std::wostream* out) : reportLevel(level), outStream(out) {};  
+	void reportEvent( E_LEVEL, std::wstring& );
+	void reportMethodFailedEvent( E_LEVEL, std::wstring&, platform::U32 );
+	void setOutputLevel( E_LEVEL );
+	void setOutput( std::wostream* );
+private:
+	E_LEVEL reportLevel;
+	std::wostream *outStream;
+	void initMap(void);
+};
+
+#endif
