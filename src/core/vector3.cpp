ccea5f8cbd9bc7f87f93728c82b77f12bc515d64
DynamiteAthe <DynamiteAthe@gmail.com>
2011-08-13 13:27:40 +0000

    Implemented Vector3 normalization.

--- src/core/vector3.cpp
+++ src/core/vector3.cpp
@@ -14,10 +14,19 @@
 //along with this program; if not, write to the Free Software
 //Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#include <platform/vtx_defineconfig.h>
 #include <core/vtx_types.h>
+
+#include <xmmintrin.h>
+#include <pmmintrin.h>
 
 using namespace core;
 using namespace platform;
+
+Vector3::Vector3()
+{
+	this->x = this->y = this->z = 0;
+}
 
 Vector3::Vector3( F32_t x, F32_t y, F32_t z )
 {
@@ -59,4 +68,35 @@ void Vector3::multiply( F32_t value, Vector3 &result )
 	result.x = this->x * value;
 	result.y = this->y * value;
 	result.z = this->z * value;
+}
+
+void Vector3::normalize(void)
+{
+	// Must pad with a trailing 0, to store in 128-bit register
+	ALIGNED_16 float vector[] = {this->x, this->y, this->z, 0};
+	__m128 simdvector;
+	__m128 result;
+	simdvector = _mm_load_ps(vector);
+	
+	// (X^2, Y^2, Z^2, 0^2)
+	result = _mm_mul_ps(simdvector, simdvector);
+
+	// Add all elements together, giving us (X^2 + Y^2 + Z^2 + 0^2)
+	result = _mm_hadd_ps(result, result);
+	result = _mm_hadd_ps(result, result);
+	
+	// Calculate square root, giving us sqrt(X^2 + Y^2 + Z^2 + 0^2)
+	result = _mm_sqrt_ps(result);
+
+	// Calculate reciprocal, giving us 1 / sqrt(X^2 + Y^2 + Z^2 + 0^2)
+	result = _mm_rcp_ps(result);
+
+	// Finally, multiply the result with our original vector.
+	simdvector = _mm_mul_ps(simdvector, result);
+
+	_mm_store_ps(vector, simdvector);
+
+	this->x = vector[0];
+	this->y = vector[1];
+	this->z = vector[2];
 }
